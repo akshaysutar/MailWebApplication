ef171217d980b4e99c234a5e36f404a550353697
Akshay Sutar <akystr@gmail.com>
2020-10-28 16:57:37 +0530

    updated mail body logic

--- MailWebApplication/Controllers/SendMailController.cs
+++ MailWebApplication/Controllers/SendMailController.cs
@@ -21,37 +21,34 @@ namespace MailWebApplication.Controllers
             var ToEmail = emailRequest.ToEmail;
             var Subject = emailRequest.Subject;
             var Body = emailRequest.Body;
-            //string textBody = "</table><tr>""<td>BLAH BLAH BLAH</td></tr></table>";
-            /**string textBody = " <table>< tr > "+
-"<th>Company Name</th>" +
-"<th>Customer Code</th>" +
-"<th>Country</th>" +
-"<th>Your Full Name</th>" +
-"<th>Position in your Company</th>" +
-"<th>Phone Number</th>" +
-"<th>Email Address</th>" +
-"</tr>" +
-"<tr>" +
-"<td>Company Name</td>" +
-"<td>Customer Code</td>" +
-"<td>Countdy</td>" +
-"<td>Your Full Name</td>" +
-"<td>Position in your Company</td>" +
-"<td>Phone Number</td>" +
-"<td>Email Address</td>" +
-"</tr></table>"  **/             ;
+              string textBody = " <table border='1'><tr> "+
+                                "<th>Company Name</th>" +
+                                "<th>Country</th>" +
+                                "<th>Your Full Name</th>" +
+                                "<th>Position in your Company</th>" +
+                                "<th>Phone Number</th>" +
+                                "<th>Email Address</th>" +
+                                "</tr>" +
+                                "<tr>" +
+                                "<td>TCS</td>" +
+                                "<td>India</td>" +
+                                "<td>Akshay Sutar</td>" +
+                                "<td>Developer</td>" +
+                                "<td>9503775217</td>" +
+                                "<td>akshay.sutar@tcs.com</td>" +
+                                "</tr></table>"              ;
             MailMessage mailMessage  = new MailMessage();
             mailMessage.Subject = Subject;
-            mailMessage.Body = Body;
+            mailMessage.Body = textBody;
             mailMessage.To.Add(ToEmail);
-            mailMessage.From = new MailAddress("FromEmailId");
-            mailMessage.IsBodyHtml = false;
+            mailMessage.From = new MailAddress("akkystr@gmail.com");
+            mailMessage.IsBodyHtml = true;
 
             SmtpClient smtp = new SmtpClient("smtp.gmail.com");
             smtp.Port = 587;
             smtp.UseDefaultCredentials = false;
             smtp.EnableSsl = true;
-            smtp.Credentials = new System.Net.NetworkCredential("FromEmailId", "Your Credentials");
+            smtp.Credentials = new System.Net.NetworkCredential("akkystr@gmail.com", "Iloveakshaysutar@2");
             smtp.Send(mailMessage);
 
             ViewBag.message = "The mail has been sent to " + mailMessage.To + "successfully";
